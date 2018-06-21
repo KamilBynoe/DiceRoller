a30cb778b8e644bc0c3443154a9e25d98f628c80
Kaybee <Kaybee@LAPTOP-0M6T1T9F.fios-router.home>
2018-06-21 13:26:11 -0400

    Basic Dice Rolling Functionality

--- DiceRoller/src/DiceRoller.java
+++ DiceRoller/src/DiceRoller.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 
 public class DiceRoller {
-
+	Random rng = new Random();
+	String ledger;
+	public static void main (String[] args) {
+	}
+	
+	public int rollDie (int dieSize) {
+		return rng.nextInt(dieSize);
+	}
+	
+	public int rollDice(int diceCount, int dieSize) {
+		int diceTotal = 0;
+		for (int i = 0; i < diceCount; i++) {
+			diceTotal += rollDie(dieSize);
+		}
+		return diceTotal;
+	}
 }
